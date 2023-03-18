4ec53fb3136d80d9b5b0508ca09c9ba3e9695053
Ojasva Raghav <Ojasraghav05@gmail.com>
2023-03-18 18:08:03 +0530

    added mob graphic to vader

--- center.py
+++ center.py
@@ -3,6 +3,21 @@ from sys import exit#Basically AllCommand of python
 from random import randint
 pygame.init()#Initializes all of pygame
 
+#Mob
+snail1=pygame.image.load('Graphics\Snail\snail1.png').convert_alpha()
+snail2=pygame.image.load('Graphics\Snail\snail2.png').convert_alpha()
+snail_frames=[snail1,snail2]
+snail_index=0
+snail_surf=snail_frames[snail_index]
+
+fly1=pygame.image.load('Graphics\Fly\Fly1.png').convert_alpha()
+fly2=pygame.image.load('Graphics\Fly\Fly2.png').convert_alpha()
+fly_frames=[fly1,fly2]
+fly_index=0
+fly_surf=fly_frames[fly_index]
+
+obstacle_rect_list=[]
+
 #Intro
 player_stand=pygame.image.load('Graphics\Player\stand1.png').convert_alpha()
 player_stand=pygame.transform.rotozoom(player_stand,0,2)
@@ -13,3 +28,4 @@ game_name_rect=game_name.get_rect(center=(400,80))
 
 game_message=text_font.render('Press space to run',False,'Red')
 game_message_rect=game_message.get_rect(center=(400,320))
+
